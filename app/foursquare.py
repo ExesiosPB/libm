e22fa6413e58601bb9a864b730df215866b3f577
Toby Drane <tobydrane@gmail.com>
2019-02-15 17:54:16 +0000

    change foursquare search

--- app/foursquare.py
+++ app/foursquare.py
@@ -12,7 +12,7 @@ AUTH_URL_PART = 'client_id={}&client_secret={}&v={}'.format(CLIENT_ID, CLIENT_SE
 # we need to do some query magic to get more than 50 results
 # https://stackoverflow.com/questions/14211120/maximum-number-of-results-in-foursquare-api
 def venuSearch(search_param):
-  url = "{}/venues/search?near={}&limit=50&{}".format(BASE_URL, search_param, AUTH_URL_PART)
+  url = "{}/venues/search?near={}&limit=50&{}&intent=browse&radius=9000".format(BASE_URL, search_param, AUTH_URL_PART)
   request = requests.get(url)
   
   # We want get lat,lng of search param from response geocode
@@ -27,7 +27,7 @@ def venuSearch(search_param):
   count = 1
   finished = False
   while finished == False:
-    req = requests.get("{}/venues/search?ll={},{}&limit=50&{}".format(BASE_URL, lat, lng, AUTH_URL_PART))
+    req = requests.get("{}/venues/search?ll={},{}&limit=50&{}&intent=browse&radius=9000".format(BASE_URL, lat, lng, AUTH_URL_PART))
     res = req.json()
     
     ven = res['response']['venues']
